23ef7bf0c1af090a34305ec6a3a957bf32befb9f
ahmed el <ahmedelmeskyny@gmail.com>
2021-03-21 15:20:22 +0100

    finish

---
+++ components/layout/layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+//components
+import Header from "../header/header";
+import Menu from "../menu/menu";
+import Footer from "../footer/footer";
+import MobileMenu from "../mobile-menu/mobile-menu"
+
+
+const Layout = ({children , isNormal}) => {
+
+
+    return (
+        <div>
+        <Header></Header>
+        <Menu isNormal={isNormal}></Menu>
+        <MobileMenu></MobileMenu>
+        {children}
+        <Footer></Footer>
+        </div>
+    )
+}
+
+export default Layout;
